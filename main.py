5054739a9f6919b84f193bb84d5f4058d1cbd688
hao.lin <hao.lin@bianlifeng.com>
2024-05-08 23:14:49 +0800

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, send_file, Response, render_template, abort
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route("/hello", methods=['GET'])
+def hello():
+    return "OK"
+
+
+if __name__ == "__main__":
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=19888)
