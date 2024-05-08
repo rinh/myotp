f27e0b502e3383e8465c1fd22afe6af93230adbc
hao.lin <hao.lin@bianlifeng.com>
2024-05-08 23:27:53 +0800

    init

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, send_file, Response, render_template, abort
 
+import pyotp
+
 app = Flask(__name__)
 
 
 @app.route("/hello", methods=['GET'])
 def hello():
     return "OK"
+
+
+@app.route("/otp", methods=['GET'])
+def otp():
+    totp = pyotp.TOTP('NSSQPXQGNJX7U6MW')
+    return str(totp.now())
 
 
 if __name__ == "__main__":
